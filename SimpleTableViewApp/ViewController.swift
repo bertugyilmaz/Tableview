4e0efe3ada822fb2ffeefab58cb51061542f43d8
onur hüseyin çantay <onurhuseyincantay@onurs-Mac.local>
2017-05-11 12:34:42 +0300

    Initial Commit

---
+++ SimpleTableViewApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleTableViewApp
+//
+//  Created by onur hüseyin çantay on 11/05/2017.
+//  Copyright © 2017 onur hüseyin çantay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
